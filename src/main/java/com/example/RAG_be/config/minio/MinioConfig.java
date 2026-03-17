7fc1b0cfb894da3155579a2ca7d0faa8ec7e7457
tuananhlaptrinh <bui058277@gmail.com>
2026-03-17 20:23:03 +0700

    fix(mino): endpoint

--- src/main/java/com/example/RAG_be/config/minio/MinioConfig.java
+++ src/main/java/com/example/RAG_be/config/minio/MinioConfig.java
@@ -1,19 +1,26 @@
 package com.example.RAG_be.config.minio;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.*;
-import java.io.*;
-
 @Configuration
 public class MinioConfig {
+  @Value("${MINIO_URL}")
+  private String minioUrl;
+
+  @Value("${MINIO_ACCESS_KEY}")
+  private String accessKey;
+
+  @Value("${MINIO_SECRET_KEY}")
+  private String secretKey;
+
   @Bean
   public MinioClient minioClient() {
     return MinioClient.builder()
-            .endpoint("http://localhost:9000")
-            .credentials("minioadmin", "minioadmin")
+            .endpoint(minioUrl)
+            .credentials(accessKey, secretKey)
             .build();
   }
 }
